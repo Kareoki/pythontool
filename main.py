c0f6df22ce632b4cb2a93a3d5216c84c07d72149
Kareoki <62131247+Kareoki@users.noreply.github.com>
2020-04-09 20:39:55 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import win32com.client as win32
+import sys
+
+
+excel = win32.gencache.EnsureDispatch
+wb = excel.Workbooks.Open("C:\\Users\\Administrator\\Desktop\\python_excel.xlsx")
+excel.Visible = True
+ws = wb.Worksheets("工作表1")
+
+
+f = open(r'C:\\Users\\Administrator\\Desktop\\Python_reading.txt')
+s = open('save.txt','a+')
+s.readline()
+text = list()
+for line in f:
+  text.append(line)
+  string = text[0]
+  str_list = string.split(',')
+  
+  if str_list[0] == "-i":
+    ws.Range(str_list[1]).Value = str_list[2]
+    s.write(str_list[1]+"儲存成功")
+    wb.Save()
+    
+  elif str_list[0] == "-r":
+    continue
+  
+  else:
+    print("wrong")
